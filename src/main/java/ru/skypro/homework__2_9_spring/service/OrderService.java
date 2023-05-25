f3bec825dd7d5a6ea65b5c67229c989c9e6477bd
am999 <am9999072080@gmail.com>
2023-05-25 23:40:56 +0300

    Commit 1

--- src/main/java/ru/skypro/homework__2_9_spring/service/OrderService.java
+++ src/main/java/ru/skypro/homework__2_9_spring/service/OrderService.java
@@ -3,5 +3,6 @@ package ru.skypro.homework__2_9_spring.service;
 import java.util.List;
 
 public interface OrderService {
-
+    List<Integer> addItems (List<Integer> ids);
+    List<Integer> getItems();
 }
--- src/main/java/ru/skypro/homework__2_9_spring/service/OrderServiceImpl.java
+++ src/main/java/ru/skypro/homework__2_9_spring/service/OrderServiceImpl.java
@@ -6,5 +6,19 @@ import ru.skypro.homework__2_9_spring.model.Cart;
 import java.util.List;
 @Service
 public class OrderServiceImpl implements OrderService{
+    private final Cart cart;
 
+    public OrderServiceImpl(Cart cart) {
+        this.cart = cart;
+    }
+
+    @Override
+    public List<Integer> addItems(List<Integer> ids) {
+        return cart.addItems(ids);
+    }
+
+    @Override
+    public List<Integer> getItems() {
+        return cart.getItems();
+    }
 }
